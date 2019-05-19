00e8c3ceaa22156500bc2167b5bb5c940483e311
kimi <mmariajessica@gmail.com>
2019-05-19 16:03:38 -0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hello rhere!')
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
